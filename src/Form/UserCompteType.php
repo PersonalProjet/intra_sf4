b694371c18ae2e61ee8d08d486e7f727a7b8bf3d
kperin <kristopher.perin@gmail.com>
2018-05-29 15:29:11 +0200

    Clean Files

---
+++ src/Form/UserCompteType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Compte;
+
+class UserCompteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', EntityType::class, [
+                'class' => Compte::class,
+                'choice_label' => function ($compte) {
+                    return $compte->getName() . ' - ' . $compte->getBanque()->getName();
+                },
+                'attr' => ['class' => 'select2'],
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
