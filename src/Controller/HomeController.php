610acc29bbca3162469331f0eac5c6879dd72d96
kperin <kristopher.perin@gmail.com>
2018-05-29 16:32:35 +0200

    Nav Banque

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,21 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    public function navigation()
+    {
+        $comptes = $this->getUser()->getCompte();
+        $banques = [];
+
+        foreach ($comptes as $compte) {
+            $banques[$compte->getBanque()->getName()][] = [
+                'id' => $compte->getId(),
+                'lib' => $compte->getName(),
+            ];
+        }
+
+        return $this->render('navigation.html.twig', [
+            'banques' => $banques,
+        ]);
+    }
 }
